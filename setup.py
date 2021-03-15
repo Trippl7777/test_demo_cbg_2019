41f75706e4d62c7d4a3c504fad354710cbad8a87
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2021-03-14 16:13:22 -1000

    new

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='demo_profile_functions',
 	version='0.1',
     description='people\'s name parser',
     author='lynzt',
-    url='https://github.com/ryanfoxsquire/safegraph_demo_profile',
+    url='https://github.com/Trippl7777/test_demo_cbg_2019',
     packages = ['demo_profile_functions']
       )
 
